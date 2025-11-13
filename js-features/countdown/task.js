70776e7caea51b7af49ae316194674e031a7d5bc
Vyacheslav <slavik.kurganov.26@mail.ru>
2025-11-13 11:52:16 +0300

    переделал задания и сделал доп

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,17 +1,32 @@
-const timerElement = document.getElementById('timer');
-let timer = 10;
-let countdoun = setInterval(() => {
-    if (timer >= 0) {
-        let timeString = new Date(timer * 1000).toLocaleTimeString('ru', {
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            timeZone: 'UTC',
-        })
-        timerElement.textContent = timeString;
-        timer--;
-    } else {
-        clearInterval(countdoun);
-        alert(`Вы победили в конкурсе!`);
-    };
-}, 1000)
+let time = 10;
+const timerElement = document.getElementById("timer");
+const hiElement = document.getElementById("Hi");
+
+const interval = setInterval(updateTimerDisplay, 1000);
+
+function updateTimerDisplay() {
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+
+    if (hours < 10){
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10){
+        minutes = "0" + minutes;
+    }
+
+    if (seconds < 10){
+        seconds = "0" + seconds;
+    }
+    timerElement.textContent = `${hours}:${minutes}:${seconds}`;
+    time--;
+
+    if (time < 0) {
+        clearInterval(interval);
+        // window.location = "HI.rtf"
+        hiElement.click();
+        alert(`Вы победили в конкурсе!`)
+    }
+}
