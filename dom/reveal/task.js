b556a508e869799d3f69b6fa1d2f9eb4a0412adc
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-02-09 13:44:09 +0300

    changed a file task.ks

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,21 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const revealElements = document.querySelectorAll('.reveal');
-
-    function revealOnScroll() {
-        revealElements.forEach(element => {
-            const elementPosition = element.getBoundingClientRect().top;
-            const viewportHeight = window.innerHeight;
-
-            // Добавляем класс, если элемент в зоне видимости
-            if (elementPosition < viewportHeight) {
-                element.classList.add('reveal_active');
-            } else {
-                element.classList.remove('reveal_active');
+document.addEventListener('DOMContentLoaded', () => {
+    const revealElement = document.querySelectorAll('.reveal');
+    document.addEventListener('scroll', () => {
+        revealElement.forEach(item => {
+            const rect = item.getBoundingClientRect();
+            if (rect.bottom >= 0 && rect.top <= window.innerHeight) {
+                item.classList.add('reveal_active')
+            }else {
+                item.classList.remove('reveal_active')
             }
-        });
-    }
-
-    // Запускаем функцию при прокрутке и при загрузке страницы
-    window.addEventListener('scroll', revealOnScroll);
-    revealOnScroll();
-});
+        })
+    });
+})
